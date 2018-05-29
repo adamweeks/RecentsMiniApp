18b466324e7eb3d3feec781fb5499d2f32c2920e
Adam Weeks <adweeks@cisco.com>
2018-05-29 17:40:09 -0400

    Initial Commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+import SpacesList from './webex/SpacesList';
+
+import testData from './testData.json';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeSpace: testData[0].id
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(space) {
+    this.setState({activeSpace: space});
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome "{this.props.username}"</Text>
+        <SpacesList
+          activeSpaceId={this.state.activeSpace}
+          onClick={this.handleClick}
+          spaces={testData}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 65
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
